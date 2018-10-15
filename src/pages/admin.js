dc150604cfd9b850e8cf0175c7a1f7c99f1e1af0
Art Pai <minipai@gmail.com>
2018-10-15 10:59:30 +0800

    add admin link

---
+++ src/pages/admin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Admin = () => (
+  <Layout>
+    <h1>管理後台</h1>
+    <p>
+      <a href="https://minipai.prismic.io">前往管理後台</a>
+    </p>
+  </Layout>
+)
+
+export default Admin
